ebb064d8d0f9d8b367db73c6e47351873c160ef8
Raed Shomali <shomali11@gmail.com>
2017-10-26 19:52:45 -0400

    Clean up

--- examples/example1.go
+++ examples/example1.go
@@ -12,8 +12,8 @@ const (
 )
 
 func main() {
-	dataSource := fmt.Sprintf(dataSourceFormat, "shomali", "", "shomali")
-	db, err := sql.Open("postgres", dataSource)
+	dataSource := fmt.Sprintf(dataSourceFormat, "<USER>", "<PASSWORD>", "<DATABASE_NAME>")
+	db, err := sql.Open("<DRIVER>", dataSource)
 	if err != nil {
 		log.Fatal(err)
 	}
